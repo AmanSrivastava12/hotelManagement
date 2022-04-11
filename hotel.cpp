98d5d03d1a3579772f5757f5acf63d452aee03b9
AmanSrivastava12 <70564443+AmanSrivastava12@users.noreply.github.com>
2022-04-11 11:20:02 +0530

    Modified the Hotel Management System file

--- hotel.cpp
+++ hotel.cpp
@@ -329,5 +329,82 @@ class HotelMgnt hm;
 int i,j,opt,rno;
 char ch;
 char pname[100];
+system("cls");
 
+do
+{
+system("cls");
+cout<<"######## Hotel Management #########\n";
+cout<<"\n1. Manage Rooms";
+cout<<"\n2. Check-In Room";
+cout<<"\n3. Available Rooms";
+cout<<"\n4. Search Customer";
+cout<<"\n5. Check-Out Room";
+cout<<"\n6. Guest Summary Report";
+cout<<"\n7. Exit";
+cout<<"\n\nEnter Option: ";
+cin>>opt;
+switch(opt)
+{
+case 1:
+manageRooms();
+break;
+case 2:
+if(count==0)
+{
+cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+getch();
+}
+else
+hm.checkIn();
+break;
+case 3:
+if(count==0)
+{
+cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+getch();
+}
+else
+hm.getAvailRoom();
+break;
+case 4:
+if(count==0)
+{
+cout<<"\nRooms are not available.\nPlease add the rooms first.";
+getch();
+}
+else
+{
+cout<<"Enter Customer Name: ";
+cin>>pname;
+hm.searchCustomer(pname);
+}
+break;
+case 5:
+if(count==0)
+{
+cout<<"\nRooms are not available.\nPlease add the rooms first.";
+getch();
+}
+else
+{
+cout<<"Enter Room Number : ";
+cin>>rno;
+hm.checkOut(rno);
+}
+break;
+case 6:
+hm.guestSummaryReport();	
+break;
+case 7:
+cout<<"\nTHANK YOU! FOR USING SOFTWARE";
+break;
+default:
+cout<<"\nPlease Enter correct option";
+break;
+}
+}while(opt!=7);
+
+getch();
+}
 
